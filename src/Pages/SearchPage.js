c50b52b832a85a3ac0e9ac39da7d19a30ddc968b
eatapancake <eat.a.pancake27@gmail.com>
2022-07-23 14:35:54 -0500

    Search Page Header

--- src/Pages/SearchPage.js
+++ src/Pages/SearchPage.js
@@ -1,9 +1,10 @@
 import React from "react";
+import SearchBar from "../Components/SearchBar";
 
 function SearchPage() {
   return (
-    <div>
-      <h2>Search Page</h2>
+    <div className="App">
+      <SearchBar header="" />
       <p>Results here...</p>
     </div>
   );
